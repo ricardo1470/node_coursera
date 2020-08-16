3e67deb460d4cd9c35108de0a13cf950f1e85d58
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-15 22:36:10 -0500

    update file

---
+++ app.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+    firts app.
+*/
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('content-Type', 'text/plain');
+  res.end('my firts server!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log('the server is running htttp://${hostname}:${port}');
+});
